1722fb0006e7252b391e8c572b7c1948ea8d045b
Ivor Thord-Gray <ivor.thordgray@gmail.com>
2023-02-01 08:05:15 +0200

    .

--- public/server.js
+++ public/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Define a basic route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server on a specified port
